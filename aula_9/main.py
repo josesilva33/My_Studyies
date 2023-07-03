61e4f395adec288f06ef857189f53d97c1c5ecf0
sergio <josenildojump521@gmail.com>
2023-07-03 17:08:33 -0300

    Meus Estudos

--- aula_9/main.py
+++ aula_9/main.py
@@ -1,3 +1,21 @@
-frase = ('curso em video phyton')
-len(frase)
-print(9)
+frase = 'Curso em Video Python'
+
+# print(frase[::2])
+# print(frase.count('o'))
+# print(frase.upper().count('O'))
+# print(len(frase))
+# print(len(frase.strip()))
+# frase = frase.replace('Python','Android')
+# print(frase)
+# print('Video' in frase)
+# print(frase.find('Video'))
+# print(frase.lower().find('Video'))
+divi = (frase.split())
+print(divi[3])
+print(divi[3][3])
+
+# print('Tal prática pode influenciar\ndiretamente a qualidade do\nseu aprendizado e reduzir sua
+# autonomia\nprofissional. Com a intenção de abordar\nquais os principais pontos ligados a\nutilização dessa
+# ferramenta e como isso\npode afetar seu desenvolvimento,\norganizamos um post detalhado sobre\no assunto. Então
+# continue a leitura\ne descubra como o ChatGPT pode afetar\nsua experiência, além de entender como\naprender
+# programação, na prática!')
